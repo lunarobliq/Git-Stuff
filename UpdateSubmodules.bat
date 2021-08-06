0356496d0095220d92a615b89702de1758bf0613
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-08-06 14:57:30 -0400

    Update UpdateSubmodules.bat

--- UpdateSubmodules.bat
+++ UpdateSubmodules.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
 
 cd %USERPROFILE%\source\repos\
-set /P reponame=Enter FORK REPO URL you want to update (YOUR FORK): 
+set /P reponame=Enter REPO  you want to update: 
 cd %USERPROFILE%\source\repos\%reponame%
+git submodule init
 git submodule foreach git pull origin master
 git commit -m "Updating the submodule 'mod' to the latest version"
 git add .
 git pull
 git push
+cd %USERPROFILE%\source\repos\
