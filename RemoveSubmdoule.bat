3dbfe6ed1ff32213a3fb9dc21f73d6efc06899c7
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-11-03 12:49:00 -0400

    Add files via upload

---
+++ RemoveSubmdoule.bat
@@ -0,0 +1,11 @@
+cd %USERPROFILE%\source\repos\
+DIR %USERPROFILE%\source\repos
+set /P reponame=Enter REPO you want to update: 
+cd %USERPROFILE%\source\repos\%reponame%
+DIR %USERPROFILE%\source\repos\%reponame%
+set /P submodule=Enter SUBMODULE you want to remove: 
+git submodule deinit %submodule%
+git rm -rf %submodule%
+git commit-m "Removed submodule"
+rm -rf .git/modules/%submodule%
+git push
