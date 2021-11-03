1332be7445c4b49f7547c61fed04c3da0ec316b5
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-11-03 13:30:15 -0400

    Update RemoveSubmdoule.bat

--- RemoveSubmdoule.bat
+++ RemoveSubmdoule.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+
 cd %USERPROFILE%\source\repos\
 DIR %USERPROFILE%\source\repos
 set /P reponame=Enter REPO you want to update: 
@@ -7,6 +8,7 @@ DIR %USERPROFILE%\source\repos\%reponame%
 set /P submodule=Enter SUBMODULE you want to remove: 
 git submodule deinit %submodule%
 git rm -rf %submodule%
-git commit-m "Removed submodule"
+git commit -m "Removed submodule"
 rm -rf .git/modules/%submodule%
 git push
+cd %USERPROFILE%\source\repos\
